9dd96fda47fe2a49b36fdaf7ead266306c75e3a1
SashaPavlovski <sasha123.ez@gmailcom>
2022-11-16 18:32:31 +0200

    home work file

---
+++ Runner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _16112022
+{
+    internal class Runner
+    {
+       
+        
+
+        public void run ()
+        {
+            Student ofek = new Student("1");
+            ofek.address = "akko";
+            ofek.name = "ofek";
+            ofek.save();
+            ofek.load();
+
+        }
+
+    }
+}
